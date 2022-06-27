fc746857f23a919ab073e7bacfd5ad5d4d5d3606
Zichun (Jerry) Gao <zgaobear@gmail.com>
2022-06-27 14:48:33 -0400

    Create squares.cpp

---
+++ C++/CCC '04 J1 - Squares/squares.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int number;
+	cin >> number;
+	cout << "The largest square has side length " << int(sqrt(number)) << '.' << '\n';
+}

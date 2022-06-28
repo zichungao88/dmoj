3d53765d39111756b7258e638638c8d29d56ded1
Zichun (Jerry) Gao <zichungao88@gmail.com>
2022-06-28 16:11:41 -0400

    Create middle.py

---
+++ Python/CCC '07 J1 - Who is in the Middle?/middle.py
@@ -0,0 +1,12 @@
+weight = []
+
+input1 = int(input())
+weight.append(input1)
+input2 = int(input())
+weight.append(input2)
+input3 = int(input())
+weight.append(input3)
+
+weight.sort()
+
+print(weight[1])
